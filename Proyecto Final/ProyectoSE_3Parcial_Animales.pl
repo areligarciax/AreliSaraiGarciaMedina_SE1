bd373a939330e316fd5e3ca089756df10a59eef1
areligarciax <124947104+areligarciax@users.noreply.github.com>
2023-12-03 00:32:13 -0600

    Create ProyectoSE_3Parcial_Animales.pl

---
+++ Proyecto Final/ProyectoSE_3Parcial_Animales.pl
@@ -0,0 +1,211 @@
+% Definir los animales en la base de datos
+animal(elefante_africano, edad(70), longitud(8), sabana, herbívoro, vertebrado, mamífero).
+animal(tigre_siberiano, edad(25), longitud(3.5), bosques_tundra, carnívoro, vertebrado, mamífero).
+animal(ballena_azul, edad(90), longitud(30), océanos, plancton, vertebrado, mamífero).
+animal(oso_pardo, edad(35), longitud(2.5), bosques_tundras, omnívoro, vertebrado, mamífero).
+animal(serpiente_boa, edad(30), longitud(4), selvas, carnívoro, vertebrado, ovíparo).
+animal(pingüino_emperador, edad(20), longitud(1.2), antártida, piscívoro, vertebrado, vivíparo).
+animal(canguro_rojo, edad(8), longitud(1.8), praderas_australianas, herbívoro, vertebrado, marsupial).
+animal(delfin_orca, edad(60), longitud(9), océanos, carnívoro, vertebrado, mamífero).
+animal(leopardo_nieve, edad(22), longitud(1.3), montañas_himalayas, carnívoro, vertebrado, mamífero).
+animal(rinoceronte_blanco, edad(40), longitud(4), sabanas, herbívoro, vertebrado, mamífero).
+animal(tortuga_marina, edad(100), longitud(2), océanos, herbívoro, vertebrado, ovíparo).
+animal(leon_marino, edad(25), longitud(2.5), regiones_polares, piscívoro, vertebrado, mamífero).
+animal(mono_capuchino, edad(45), longitud(0.5), selvas, omnívoro, vertebrado, mamífero).
+animal(armadillo, edad(15), longitud(0.6), bosques, insectívoro, vertebrado, mamífero).
+animal(aguila_calva, edad(30), longitud(2.3), américa_del_norte, carnívoro, vertebrado, ovíparo).
+animal(pulpo_comun, edad(3), longitud(1), océanos, carnívoro, invertebrado, ovíparo).
+animal(panda_gigante, edad(30), longitud(1.8), bosques_bambú, herbívoro, vertebrado, mamífero).
+animal(tucan, edad(20), longitud(0.6), selvas, frugívoro, vertebrado, ovíparo).
+animal(coyote, edad(14), longitud(1.1), américa_del_norte, omnívoro, vertebrado, mamífero).
+animal(camello, edad(40), longitud(3), desiertos, herbívoro, vertebrado, mamífero).
+animal(cocodrilo_del_nilo, edad(70), longitud(5), ríos_manglares, carnivoro, vertebrado, ovíparo).
+animal(cebra, edad(25), longitud(2.5), sabanas_praderas, herbívoro, vertebrado, mamífero).
+animal(buho_real, edad(25), longitud(0.7), bosques, carnívoro, vertebrado, ovíparo).
+animal(lince_iberico, edad(15), longitud(1.2), bosques_montañas, carnivoro, vertebrado, mamífero).
+animal(gorila, edad(50), longitud(1.7), bosques_tropicales, herbívoro, vertebrado, mamífero).
+animal(tiburon_ballena, edad(70), longitud(18), océanos, carnivoro, vertebrado, ovíparo).
+animal(mono_howler, edad(20), longitud(0.7), selvas, herbívoro, vertebrado, mamífero).
+animal(elefante_indio, edad(60), longitud(5.5), bosques, herbívoro, vertebrado, mamífero).
+animal(jirafa, edad(25), longitud(5.5), sabanas_bosques, herbívoro, vertebrado, mamífero).
+animal(camaleon, edad(5), longitud(0.3), selvas_bosques, insectívoro, vertebrado, ovíparo).
+animal(dingo, edad(15), longitud(1.2), australia, carnivoro, vertebrado, mamífero).
+animal(oso_panda_rojo, edad(14), longitud(0.6), bosques_bambú, herbívoro, vertebrado, mamífero).
+animal(pingüino_adelaida, edad(23), longitud(1), antártida, piscívoro, vertebrado, vivíparo).
+animal(langosta, edad(10), longitud(0.3), océa.nos_mares, omnívoro, invertebrado, ovíparo).
+animal(cangrejo_ermitano, edad(30), longitud(0.3), océanos_mares, omnívoro, invertebrado, ovíparo).
+animal(murcielago, edad(30), longitud(0.25), diversos_hábitats, insectívoro, vertebrado, vivíparo).
+animal(tigre_bengala, edad(20), longitud(3.1), bosques_tropicales, carnivoro, vertebrado, mamífero).
+animal(colibri, edad(5), longitud(0.1), bosques, nectarívoro, vertebrado, ovíparo).
+animal(hipopotamo, edad(50), longitud(4), ríos_lagos, herbívoro, vertebrado, mamífero).
+animal(camello_bactriano, edad(40), longitud(3.5), desiertos, herbívoro, vertebrado, mamífero).
+animal(murcielago_vampiro, edad(20), longitud(0.09), bosques, carnivoro, vertebrado, vivíparo).
+animal(caballito_de_mar, edad(5), longitud(0.15), océanos_mares, carnivoro, vertebrado, ovíparo).
+animal(avestruz, edad(40), longitud(2.7), sabanas, omnívoro, vertebrado, ovíparo).
+animal(kiwi, edad(20), longitud(0.45), bosques_nueva_zelanda, omnívoro, vertebrado, ovíparo).
+animal(ocelote, edad(15), longitud(1.1), selvas_américa, carnívoro, vertebrado, mamífero).
+animal(cebadilla, edad(5), longitud(0.18), praderas_europa, herbívoro, invertebrado, ovíparo).
+animal(gacela, edad(12), longitud(1.2), sabanas_áfrica, herbívoro, vertebrado, mamífero).
+animal(avispa, edad(1), longitud(0.02), jardines_bosques, carnívoro, invertebrado, ovíparo).
+animal(tejón, edad(14), longitud(0.7), bosques_américa, omnívoro, vertebrado, mamífero).
+animal(murciélago_frugívoro, edad(10), longitud(0.12), bosques_sudamérica, frugívoro, vertebrado, vivíparo).
+animal(tortuga_terrestre, edad(150), longitud(0.5), bosques_desiertos, herbívoro, vertebrado, ovíparo).
+animal(foca_gris, edad(35), longitud(2), regiones_árticas, piscívoro, vertebrado, mamífero).
+animal(hiena, edad(25), longitud(1), sabanas_bosques, carnívoro, vertebrado, mamífero).
+animal(águila_culebrera, edad(20), longitud(1.5), bosques_montañas, carnívoro, vertebrado, ovíparo).
+animal(lagarto_verde, edad(10), longitud(0.5), bosques_praderas, insectívoro, vertebrado, ovíparo).
+animal(mono_titi, edad(15), longitud(0.3), selvas, frugívoro, vertebrado, vivíparo).
+animal(ácaro, edad(0.5), longitud(0.001), diversos_hábitats, omnívoro, invertebrado, ovíparo).
+animal(caballo, edad(30), longitud(1.8), praderas, herbívoro, vertebrado, mamífero).
+animal(camarón, edad(6), longitud(0.15), océanos_mares, omnívoro, invertebrado, ovíparo).
+animal(coala, edad(20), longitud(0.8), bosques_australia, herbívoro, vertebrado, mamífero).
+animal(perro_salvaje, edad(14), longitud(1), selvas_desiertos, carnívoro, vertebrado, mamífero).
+animal(lombriz_tierra, edad(5), longitud(0.2), suelos, detritívoro, invertebrado, ovíparo).
+animal(girgol, edad(8), longitud(0.1), bosques_europa, omnívoro, invertebrado, ovíparo).
+animal(perdiz, edad(5), longitud(0.4), praderas, herbívoro, vertebrado, ovíparo).
+animal(medusa_luminiscente, edad(1), longitud(0.02), océanos_mares, carnívoro, invertebrado, ovíparo).
+animal(suricata, edad(10), longitud(0.3), sabanas, omnívoro, vertebrado, mamífero).
+animal(tritón, edad(8), longitud(0.15), estanques_bosques, carnívoro, vertebrado, ovíparo).
+animal(marabú, edad(25), longitud(1.6), sabanas, carnívoro, vertebrado, ovíparo).
+animal(salamanquesa, edad(7), longitud(0.15), bosques, insectívoro, vertebrado, ovíparo).
+animal(camarón_mantis, edad(4), longitud(0.2), océanos_mares, carnívoro, invertebrado, ovíparo).
+animal(rinoceronte_negro, edad(40), longitud(3), sabanas, herbívoro, vertebrado, mamífero).
+animal(mantis_religiosa, edad(1), longitud(0.1), jardines_praderas, carnívoro, invertebrado, ovíparo).
+animal(hormiga_cosechadora, edad(1), longitud(0.01), bosques, herbívoro, invertebrado, ovíparo).
+animal(cóndor, edad(70), longitud(3), cordilleras, carnívoro, vertebrado, ovíparo).
+animal(mosquito, edad(0.1), longitud(0.01), diversos_hábitats, hematófago, invertebrado, ovíparo).
+animal(nutria, edad(15), longitud(1), ríos_lagos, carnívoro, vertebrado, mamífero).
+animal(abeja, edad(1), longitud(0.02), jardines_praderas, herbívoro, invertebrado, ovíparo).
+animal(tapir, edad(30), longitud(2), bosques_ríos, herbívoro, vertebrado, mamífero).
+animal(calamar_gigante, edad(3), longitud(10), océanos, carnívoro, invertebrado, ovíparo).
+animal(chimpance, edad(50), longitud(1.5), bosques_tropicales, omnívoro, vertebrado, mamífero).
+animal(tarántula, edad(25), longitud(0.2), selvas, carnívoro, invertebrado, ovíparo).
+animal(ganso, edad(20), longitud(1), lagos_praderas, herbívoro, vertebrado, ovíparo).
+animal(mantis_orquidea, edad(1), longitud(0.1), bosques, carnívoro, invertebrado, ovíparo).
+animal(murciélago_fruitivo, edad(15), longitud(0.1), bosques_américa, frugívoro, vertebrado, vivíparo).
+animal(lince_canadiense, edad(18), longitud(1.2), bosques_américa, carnívoro, vertebrado, mamífero).
+animal(ratón_campesino, edad(3), longitud(0.2), campos, omnívoro, vertebrado, vivíparo).
+animal(elefante_de_bosque, edad(60), longitud(4.5), bosques, herbívoro, vertebrado, mamífero).
+animal(gaviota_reidora, edad(15), longitud(0.6), costas_océanos, piscívoro, vertebrado, ovíparo).
+animal(mono_capuchino_enano, edad(20), longitud(0.4), selvas, omnívoro, vertebrado, mamífero).
+animal(escarabajo_rinoceronte, edad(2), longitud(0.05), bosques, detritívoro, invertebrado, ovíparo).
+animal(lagarto_cornudo, edad(5), longitud(0.3), desierto, insectívoro, vertebrado, ovíparo).
+animal(cóndor_andino, edad(70), longitud(3.5), cordilleras, carronero, vertebrado, ovíparo).
+animal(ciervo_colorado, edad(15), longitud(2), bosques_praderas, herbívoro, vertebrado, mamífero).
+animal(cangrejo_fiddler, edad(3), longitud(0.1), manglares, omnívoro, invertebrado, ovíparo).
+animal(puma, edad(18), longitud(2.5), bosques_américa, carnívoro, vertebrado, mamífero).
+animal(saltamontes, edad(1), longitud(0.05), praderas_bosques, herbívoro, invertebrado, ovíparo).
+animal(nutria_marina, edad(20), longitud(1.5), océanos_mares, piscívoro, vertebrado, mamífero).
+animal(murciélago_insectívoro, edad(12), longitud(0.15), bosques, insectívoro, vertebrado, vivíparo).
+animal(lechuza_común, edad(8), longitud(0.4), bosques_praderas, carnívoro, vertebrado, ovíparo).
+animal(serpiente_coralillo, edad(12), longitud(0.9), bosques, carnívoro, vertebrado, ovíparo).
+animal(jaguarundi, edad(10), longitud(1.2), selvas_américa, carnívoro, vertebrado, mamífero).
+animal(gorgojo, edad(1), longitud(0.02), bosques_praderas, herbívoro, invertebrado, ovíparo).
+animal(lémur_ratón, edad(12), longitud(0.3), bosques_madagascar, omnívoro, vertebrado, mamífero).
+animal(gorrión, edad(3), longitud(0.15), ciudades_praderas, granívoro, vertebrado, ovíparo).
+animal(camarón_limpiador, edad(3), longitud(0.1), océanos_mares, carnívoro, invertebrado, ovíparo).
+animal(escarabajo_tigre, edad(2), longitud(0.06), bosques, carnívoro, invertebrado, ovíparo).
+animal(avión_trepador, edad(6), longitud(0.2), bosques_américa, insectívoro, vertebrado, ovíparo).
+animal(mantis_asiática, edad(1), longitud(0.1), jardines_praderas, carnívoro, invertebrado, ovíparo).
+animal(cucaracha, edad(1), longitud(0.02), diversos_hábitats, detritívoro, invertebrado, ovíparo).
+animal(canguro_arborícola, edad(15), longitud(1.2), bosques_australia, herbívoro, vertebrado, marsupial).
+animal(leopardo_nublado, edad(15), longitud(0.9), selvas_asia, carnívoro, vertebrado, mamífero).
+animal(pato_mandarín, edad(10), longitud(0.6), lagos_praderas, herbívoro, vertebrado, ovíparo).
+animal(águila_pescadora, edad(25), longitud(0.6), costas_océanos, piscívoro, vertebrado, ovíparo).
+animal(lagartija_espinosa, edad(5), longitud(0.15), desiertos, insectívoro, vertebrado, ovíparo).
+animal(escarabajo_lucero, edad(2), longitud(0.05), bosques_praderas, omnívoro, invertebrado, ovíparo).
+animal(lince_roussillon, edad(12), longitud(0.8), bosques_europa, carnívoro, vertebrado, mamífero).
+animal(saltarina_raya, edad(1), longitud(0.03), selvas, herbívoro, invertebrado, ovíparo).
+animal(elefante_asiático, edad(60), longitud(5), bosques, herbívoro, vertebrado, mamífero).
+animal(mono_colobus, edad(25), longitud(0.8), bosques_áfrica, herbívoro, vertebrado, mamífero).
+animal(colibrí_rabadilla, edad(5), longitud(0.1), bosques, nectarívoro, vertebrado, ovíparo).
+animal(trucha_arcoiris, edad(7), longitud(0.6), ríos_lagos, piscívoro, vertebrado, ovíparo).
+animal(león_africano, edad(20), longitud(2.5), sabanas, carnívoro, vertebrado, mamífero).
+animal(mariposa_monarca, edad(1), longitud(0.1), jardines_praderas, nectarívoro, invertebrado, ovíparo).
+animal(delfín_rosado, edad(20), longitud(2), ríos_américa, piscívoro, vertebrado, mamífero).
+animal(mosca, edad(0.1), longitud(0.01), diversos_hábitats, detritívoro, invertebrado, ovíparo).
+animal(tortuga_galápagos, edad(150), longitud(1.2), islas_galápagos, herbívoro, vertebrado, ovíparo).
+animal(coyote_africano, edad(15), longitud(1), sabanas, carnívoro, vertebrado, mamífero).
+animal(langosta_espinosa, edad(8), longitud(0.25), océanos_mares, omnívoro, invertebrado, ovíparo).
+animal(zorro_volador, edad(12), longitud(0.25), bosques_asia, insectívoro, vertebrado, vivíparo).
+animal(gato_doméstico, edad(18), longitud(0.5), hogares, omnívoro, vertebrado, mamífero).
+animal(anguila_eléctrica, edad(15), longitud(2), ríos_américa, carnívoro, vertebrado, ovíparo).
+animal(mariposa_tigre, edad(2), longitud(0.1), jardines_praderas, nectarívoro, invertebrado, ovíparo).
+animal(tigre_blancanieves, edad(20), longitud(3), bosques, carnívoro, vertebrado, mamífero).
+animal(tritón_alpino, edad(10), longitud(0.15), ríos_montañas, carnívoro, vertebrado, ovíparo).
+animal(cabra_montés, edad(15), longitud(1), montañas, herbívoro, vertebrado, mamífero).
+animal(serpiente_ratón, edad(10), longitud(1.2), praderas, carnívoro, vertebrado, ovíparo).
+animal(pingüino_humboldt, edad(20), longitud(0.7), costas_américa, piscívoro, vertebrado, ovíparo).
+animal(escarabajo_madera, edad(1), longitud(0.02), bosques, detritívoro, invertebrado, ovíparo).
+animal(gorila_de_montaña, edad(40), longitud(1.8), montañas, herbívoro, vertebrado, mamífero).
+animal(ratón_canguro, edad(5), longitud(0.1), bosques_asia, herbívoro, vertebrado, ovíparo).
+animal(delfín_blanco, edad(40), longitud(4), océanos, piscívoro, vertebrado, mamífero).
+animal(libélula, edad(1), longitud(0.1), ríos_lagos, carnívoro, invertebrado, ovíparo).
+animal(tortuga_caja, edad(100), longitud(0.3), bosques_praderas, herbívoro, vertebrado, ovíparo).
+animal(cóndor_californiano, edad(50), longitud(2.5), montañas, carronero, vertebrado, ovíparo).
+animal(lémur_coronado, edad(15), longitud(0.5), bosques_madagascar, omnívoro, vertebrado, mamífero).
+animal(escarabajo_dorado, edad(2), longitud(0.02), bosques, omnívoro, invertebrado, ovíparo).
+animal(camaleón_veiled, edad(6), longitud(0.5), bosques, insectívoro, vertebrado, ovíparo).
+animal(hormiga_carpintera, edad(1), longitud(0.02), bosques, herbívoro, invertebrado, ovíparo).
+animal(tucán_toco, edad(20), longitud(0.6), selvas_américa, frugívoro, vertebrado, ovíparo).
+animal(ciervo_sambar, edad(20), longitud(2.5), bosques_asia, herbívoro, vertebrado, mamífero).
+animal(saltamontes_rosado, edad(1), longitud(0.05), praderas, herbívoro, invertebrado, ovíparo).
+animal(hiena_risueña, edad(15), longitud(1), sabanas, carnívoro, vertebrado, mamífero).
+animal(abejorro, edad(1), longitud(0.03), jardines_praderas, herbívoro, invertebrado, ovíparo).
+animal(murciélago_zorro, edad(10), longitud(0.2), bosques, insectívoro, vertebrado, vivíparo).
+animal(tortuga_pintada, edad(70), longitud(1.2), bosques_ríos, herbívoro, vertebrado, ovíparo).
+animal(mono_dorado, edad(25), longitud(0.5), selvas_américa, omnívoro, vertebrado, mamífero).
+animal(lagartija_piedra, edad(8), longitud(0.2), desiertos, insectívoro, vertebrado, ovíparo).
+animal(mariposa_almirante, edad(2), longitud(0.05), jardines_praderas, nectarívoro, invertebrado, ovíparo).
+animal(león_marinomuscular, edad(25), longitud(2), océanos, piscívoro, vertebrado, mamífero).
+animal(abeja_albañil, edad(1), longitud(0.02), jardines_praderas, herbívoro, invertebrado, ovíparo).
+animal(pavo_real, edad(20), longitud(1.2), bosques, omnívoro, vertebrado, ovíparo).
+animal(murciélago_pescador, edad(12), longitud(0.15), bosques, piscívoro, vertebrado, vivíparo).
+
+% Reglas para obtener información de un animal específico
+especie(Especie, Animal) :- animal(Animal, _, _, _, _, _, _), Especie = Animal.
+nombre_animal(Nombre, Edad, Longitud, Habitat, Alimento, Vertebrado, Reproduccion):- animal(Nombre, edad(Edad), longitud(Longitud), Habitat, Alimento, Vertebrado, Reproduccion).
+edad(Edad, Animal) :- animal(Animal, edad(Edad), _, _, _, _, _).
+longitud(Longitud, Animal) :- animal(Animal, _, longitud(Longitud), _, _, _, _).
+habitat(Habitat, Animal) :- animal(Animal, _, _, Habitat, _, _,_).
+alimento(Alimento, Animal) :- animal(Animal, _, _, _, Alimento, _, _).
+vertebrado(Vertebrado, Animal) :- animal(Animal, _, _, _, _, Vertebrado, _).
+reproduccion(Reproduccion, Animal) :- animal(Animal, _, _, _, _, _,Reproduccion).
+
+
+% -------------------------------------------------------
+% Motor de inferencia simple para un sistema experto
+
+% Regla para determinar si un animal es herbívoro
+es_herbivoro(Animal) :-
+    animal(Animal, _, _, _, herbivoro, _, _).
+
+% Regla para determinar si un animal es carnívoro
+es_carnivoro(Animal) :-
+    animal(Animal, _, _, _, carnivoro, _, _).
+
+% Regla para determinar si un animal es omnívoro
+es_omnivoro(Animal) :-
+    animal(Animal, _, _, _, omnivoro, _, _).
+
+% Regla para determinar si un animal es un mamífero
+es_mamifero(Animal) :-
+    animal(Animal, _, _, _, _, vertebrado, mamifero).
+
+% Regla para determinar si un animal es un reptil
+es_reptil(Animal) :-
+    animal(Animal, _, _, _, _, vertebrado, oviparo).
+
+% Regla para determinar si un animal es un ave
+es_ave(Animal) :-
+    animal(Animal, _, _, _, _, vertebrado, oviparo).
+
+% Regla para determinar si un animal es un invertebrado
+es_invertebrado(Animal) :-
+    animal(Animal, _, _, _, _, invertebrado, oviparo).
+
+% Regla para determinar si un animal es acuático
+es_acuatico(Animal) :-
+    animal(Animal, _, _, oceanos, _, _, _).
+es_acuatico(Animal) :-
+    animal(Animal, _, _, rios_manglares, _, _, _).
